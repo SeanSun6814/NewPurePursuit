500eea19069e782cf9ec66e9c88546cac77c4cd3
SeanSun6814 <SeanSun6814@users.noreply.github.com>
2019-11-03 17:08:15 -0800

    Filename fixed

--- src/main/java/frc/robot/Logger.java
+++ src/main/java/frc/robot/Logger.java
@@ -25,9 +25,9 @@ public class Logger {
 
     public Logger() {
         try {
-            String time = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss'.txt'").format(new Date());
-            writer = new PrintWriter("/home/lvuser/Log" + time + ".csv");
-            // writer = new PrintWriter("./Log" + time + ".csv");
+            String time = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(new Date());
+            // writer = new PrintWriter("/home/lvuser/Log" + time + ".csv");
+            writer = new PrintWriter("./Log " + time + ".csv");
             writer.println("Timestamp, Title, Message");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
